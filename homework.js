837ac40e3469df3e029a86cb81bdcfeb45d5bcde
Eduardo5011 <eduardo.rosado5011@gmail.com>
2021-11-14 22:06:40 -0500

    update

--- homework.js
+++ homework.js
@@ -7,14 +7,19 @@ let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
 let dog_names = ["Max","HAS","PuRple","dog"]
 
 function findWords(){
-    for(let i = 0; i <dog_names.length; i+2){
-        if( i % 2 == 0){
+    for(let i = 0; i <dog_names.length; i++){
+        if(dog_string.includes(dog_names[i])){
             dog_string.includes(i,1, "even index")
+            console.log("Matched dog_name")
+            
+        }
+        else{
+            console.log("no matches")
         }
     }
 }
-replaceEvens()
-console.log()
+findWords()
+
    
 
 //============Exercise #2 ============//
@@ -24,14 +29,15 @@ and replaces it with the string "even index" */
 let arr = ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
 
 function replaceEvens(arr){
-    for(let i = 0; i <arr.length; i+1){
+    for(let i = 0; i <arr.length; i++){
         if( i % 2 == 0){
             arr.splice(i,1, "even index")
         }
     }
+    return arr
 }
-replaceEvens()
-console.log(arr)
+
+console.log(replaceEvens(arr))
 
 
     
